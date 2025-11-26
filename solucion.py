bb7043f9dd28391c65189a3bfbaa226f6ff33300
KricoFR <kristophex5@gmail.com>
2025-11-26 14:47:56 -0700

    Improve height validation in reloj_arena function
    Updated validation to check for non-integer input.

--- solucion.py
+++ solucion.py
@@ -2,7 +2,7 @@
 
 def reloj_arena(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
-    if m <= 0:
+    if m <= 0 or m=="0":
         print("Error la altura debe ser mayor a cero")
         return
         # Parte 1: Triangulo creciente
