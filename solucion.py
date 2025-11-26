c2be048464b3a2074739bac76598275c0931e4e3
KricoFR <kristophex5@gmail.com>
2025-11-26 14:10:14 -0700

    Implement height validation in reloj_arena
    Added validation for height in reloj_arena function.

--- solucion.py
+++ solucion.py
@@ -2,6 +2,14 @@
 
 def reloj_arena(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
-    
+    if m <= 0:
+        Print("Error la altura debe ser mayor a cero")
+        return
+        # Parte 1: Triangulo creciente
+    for i in range(1, m + 1):
+        print(s * i)
+        # Parte 2: Triangulo decreciente    
+    for i in range(m - 1, 0 -1):
+        print(s * i)
     # TODO: implementar la lógica para generar el reloj de arena en ASCII
     pass
